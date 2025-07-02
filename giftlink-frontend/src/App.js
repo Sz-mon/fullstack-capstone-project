755fe36dcf6cacb5c464051a98a19f57ec19f0fb
Szymon <szymon.alachamowicz@akersolutions.com>
2025-07-02 01:16:57 -0400

    Navigation bar

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,10 +4,11 @@ import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import SearchPage from './components/SearchPage/SearchPage';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 function App() {
   const navigate = useNavigate();
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
           <Route path="/app/profile" element={<Profile/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
   );
